a813c6e7481605f697e463315eadc891eee0882c
Sharma <Shashwat.Sharma@cdk.com>
2023-11-18 23:04:24 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "shashwatshrm@gmail.com"
+var accountPassword ="12345"
+accountCity = "chamba"
+
+//accountId =2 
+accountEmail ="abc@gmail.com"
+console.log(accountId)
+
+console.table([accountId, accountEmail,accountPassword,accountCity])
+
+/*
+We dont use var much because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3+ 3)  // thats not work in nodeJs
+
+let name = "shashwat"
+let age = 24
+let isLoggedIn= false
+
+//number=> 2 to the power 53
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof null) // object (it could be language error)
