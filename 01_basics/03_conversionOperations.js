f811fb113d6e23e11558ec21c726dc582f9c3a7f
Sharma <Shashwat.Sharma@cdk.com>
2023-11-19 22:40:28 +0530

    new changes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,45 @@
+/*
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+*/
+//"33" => 33
+//"33abc" => NaN
+//true =>1; false => 0
+/*
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+*/
+//1 => true; 0 =>false
+//"" =>false; "abc"=>true
+
+
+//*********************************************** OPERATIONS *******************************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let str1 ="hello"
+let str2 =" hitesh"
+
+let str3 =str1+str2
+
+console.log(str3)
+
+console.log("1" +2) //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+console.log(1 + 2 + "2" + 2); //322
+
+let gameCounter =100
+console.log(gameCounter++)
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,12 @@
+// Primitive
+
+// 7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+const id = Symbol ('123')
+const anotherId = Symbol ('123')
+
+console.log(id === anotherId);
+
+// Reference Type (Non Primitive)
+
+// Array, Objects, Functions
